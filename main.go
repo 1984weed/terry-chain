cb8ef99683ac35e785a2c14c87732cb09df18554
Teruo Kunihiro <yuyuyu1999@gmail.com>
2019-07-31 21:55:54 +0900

    Update

--- main.go
+++ main.go
@@ -2,22 +2,36 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 type Block struct {
 	Index     int
 	Previous  string
 	Timestamp int64
-	data      interface{}
-	hash      string
+	Data      string
+	Hash      string
 }
 
-func generateNextBlock(blockData string) {
-	previousBlock := getltestBlock()
-	nextIndex := previousBlock.Index + 1
-	newxtTimestamp = time.Now()
+func NewBlock(index int, hash, previousHash string, timestamp int64, data string) *Block {
+	return &Block{
+		Index:     index,
+		Previous:  previousHash,
+		Timestamp: timestamp,
+		Data:      data,
+		Hash:      hash,
+	}
 }
+
+func CalculateHash (index: number, previousHash: string, timestamp: number, data: string): string {
+	return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+}
+
+
+// func generateNextBlock(blockData string) {
+// 	previousBlock := getltestBlock()
+// 	nextIndex := previousBlock.Index + 1
+// 	newxtTimestamp = time.Now()
+// }
 
 func main() {
 	fmt.Print("======")
